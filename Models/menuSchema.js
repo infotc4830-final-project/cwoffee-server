0f78a97feeb414c5ae2802db707b1dd00d3bfc32
ericstran <ericstran.stl@gmail.com>
2023-05-03 22:58:27 -0500

    updated menuSchema

--- Models/menuSchema.js
+++ Models/menuSchema.js
@@ -3,7 +3,16 @@ const mongoose = require('mongoose')
 const menuSchema = mongoose.Schema({
 	_id: { type: mongoose.ObjectId },
 	name: { type: String },
-	type: { type: String, enum: ['DRINK', 'FOOD'], default: 'DRINK' },
+	price: { type: String },
+	description: { type: String },
+	options: [
+		{
+			style: { type: String },
+			price: { type: String },
+		},
+	],
+	type: { type: String },
+	category: { type: String },
 })
 
 module.exports = mongoose.model('menus', menuSchema)
