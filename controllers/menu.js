9c43231e613172711b70171c2e6c7a230dd74c73
jstepanek <55816220+jstepanek@users.noreply.github.com>
2023-05-03 22:11:40 -0500

    menu controller refactor

--- controllers/menu.js
+++ controllers/menu.js
@@ -1,6 +1,6 @@
 const MenuModel = require('../Models/menuSchema')
 
-const handleMenuGetAll = async (req, res) => {
+const handleMenuGetAllItems = async (req, res) => {
 	let data
 	try {
 		data = await MenuModel.find({})
@@ -11,7 +11,8 @@ const handleMenuGetAll = async (req, res) => {
 	return res.status(200).json({ message: 'success', data: data })
 }
 
-const handleMenuGetOne = async (req, res) => {
+const handleMenuPostOneItem = async (req, res) => {
+
 	if (!req.body.name)
 		return res.status(400).json({ message: 'missing inputs' })
 
@@ -25,23 +26,7 @@ const handleMenuGetOne = async (req, res) => {
 	return res.status(200).json({ message: 'success', data: data })
 }
 
-// const handleMenuPostOne = async (req, res) => {
-
-// 	// if (!req.body.name)
-// 	// 	return res.status(400).json({ message: 'missing inputs' })
-
-// 	let data
-// 	try {
-// 		data = await MenuModel.find({ name: req.body.name })
-// 	} catch (e) {
-// 		return res.status(500).json({ message: 'failed test connection' })
-// 	}
-
-// 	return res.status(200).json({ message: 'success', data: data })
-// }
-
 module.exports = {
-	handleMenuGetAll,
-	handleMenuGetOne,
-	// handleMenuPostOne,
+	handleMenuGetAllItems,
+	handleMenuPostOneItem
 }
