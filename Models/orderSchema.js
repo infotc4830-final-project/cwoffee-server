abf4540342df4a1b2fb1e64a13bef8c901a0e57e
ericstran <ericstran.stl@gmail.com>
2023-05-04 21:58:43 -0500

    order items now an array of strings rather than objectIds

--- Models/orderSchema.js
+++ Models/orderSchema.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const orderSchema = mongoose.Schema({
 	orderId: { type: mongoose.ObjectId, required: true },
 	uid: { type: mongoose.ObjectId, required: true },
-	items: { type: [mongoose.ObjectId], required: true },
+	items: [{ type: String }],
 	totalPrice: { type: String, required: true },
 })
 
